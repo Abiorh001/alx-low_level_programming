a404d520c82eb952d0bbd72c3b82cb1c718b6e78
Abiola <abiolaadedayo1993@gmail.com>
2022-09-08 13:25:18 -0700

    soution to hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - main block
+ * Return: 1
  */
 int main(void)
 {
-	fprintf(
-		stderr, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n"
-	       );
+	int s = strlen("and that piece of art is useful - Dora korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful - Dora korpar, 2015-10-19\n";
+	      s);
 	return (1);
 }
