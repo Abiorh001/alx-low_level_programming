d6b93b788d71f70c830e6790ce61d807e44a1300
Abiola <abiolaadedayo1993@gmail.com>
2022-09-13 06:47:11 -0700

    solution to function

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+/**
+ * File: main.h
+ * Author: Abiola Adeshina
+ * Description: Header file containing declaration for all functions used in the 0x02-functions_nested_loops directory
+ *
+ */
+int _putchar(char c);
+int main(void);
+
+#endif
