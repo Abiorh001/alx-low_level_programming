371e00735a05ca72270c3f12b56438179a9aeac2
Abiola <abiolaadedayo1993@gmail.com>
2022-09-14 02:32:15 -0700

    solution to debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - test positive_or_negative(i)
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positve_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - causes an infinite loop
+ * man: causes an infinite loop
+ * Return: 0
+ */
+
+int man(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+	/*
+	 * while (i < 10)
+	 *
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\m");
+
+	return (0);
+}
