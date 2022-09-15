51ac71f77c0efce721159e613d4f1abf74fce2ab
Abiola <abiolaadedayo1993@gmail.com>
2022-09-15 14:29:46 -0700

    nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,17 +2,10 @@
 /**
  * _isupper - evaluate if letter is uppercase
  * @c: print int
- * Return: Always 0
+ * Return: 1 or 0
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return ((c >= 65) && (c <= 90));
 }
